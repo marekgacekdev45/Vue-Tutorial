571d9f26260747aaeb0e05bc9c1cc5dd0b73a72d
marekgacek45 <marekgacek45@gmail.com>
2023-07-05 13:03:29 +0200

    working on project

---
+++ modal-project/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+	<div>
+		<h1>{{ title }}</h1>
+		<teleport to=".modals" v-if="showModal">
+			<ModalComponent :header="header" theme="sale" @close="toggleModal">
+				<template v-slot:links>
+					<div class="actions">
+						<a href="#">sign in</a>
+						<a href="#">register</a>
+					</div>
+				</template>
+				<p>
+					treść modala Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deleniti maiores repudiandae
+					labore, atque delectus eius enim quia exercitationem voluptates?
+				</p>
+			</ModalComponent>
+		</teleport>
+		<teleport to=".modals" v-if="showModal2">
+			<ModalComponent @close="toggleModal2"><p>
+					treść modala
+				</p> </ModalComponent>
+		</teleport>
+		<button @click="toggleModal">Open</button>
+		<button @click="toggleModal2">Open Modal 2</button>
+	</div>
+</template>
+
+<script>
+import ModalComponent from './components/ModalComponent.vue'
+
+export default {
+	name: 'App',
+	components: {
+		ModalComponent,
+	},
+	data() {
+		return {
+			title: 'jakiś tytuł',
+			header: 'Unikatowy modal',
+			// text: 'treść modala',
+			showModal: false,
+			showModal2: false,
+		}
+	},
+	methods: {
+		handleClick() {
+			console.log(this.$refs.name)
+			const input = this.$refs.name
+			input.classList.add('active')
+			input.focus()
+		},
+		toggleModal() {
+			this.showModal = !this.showModal
+		},
+		toggleModal2() {
+			this.showModal2 = !this.showModal2
+		},
+	},
+}
+</script>
+
+<style>
+#app, .modals {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+h1 {
+	border-bottom: 1px solid rebeccapurple;
+	padding-bottom: 10px;
+	display: inline-block;
+}
+</style>
---
+++ modal-project/src/components/ModalComponent.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="backdrop" @click.self="closeModal">
+		<div class="modal" :class="{ sale: theme === 'sale' }">
+			<h1>{{ header }}</h1>
+			<slot>default content</slot>
+				<slot name="links"></slot>
+			
+			<button @click="closeModal" >closeModal</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['header', 'text', 'theme'],
+	methods: {
+		closeModal() {
+			this.$emit('close')
+		},
+	},
+}
+</script>
+
+<style >
+.modal {
+	width: 400px;
+	padding: 20px;
+	margin: 100px auto;
+	background: white;
+	border-radius: 10px;
+}
+.backdrop {
+	top: 0;
+	position: fixed;
+	background: rgba(0, 0, 0, 0.5);
+	width: 100%;
+	height: 100%;
+}
+.modal .sale {
+	background-color: yellowgreen;
+	color: white;
+}
+.modal .sale h1 {
+	color: darkblue;
+}
+.modal h1 {
+	color: turquoise;
+	border: none;
+}
+.modal .actions{
+	text-align: center;
+	margin: 30px 0 10px 0;
+	
+}
+.modal .actions a{
+	color:#333;
+	padding: 8px;
+	border: 1px solid red;
+	border-radius: 4px;
+	text-decoration: none;
+	margin: 10px;
+}
+</style>
